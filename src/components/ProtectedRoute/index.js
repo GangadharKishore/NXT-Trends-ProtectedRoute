9d3b00c3c674b4ce953695dcbe28d745f73f0477
GangadharKishore <gkishore845@gmail.com>
2023-10-27 06:51:05 +0000

    ProtectedRoute

---
+++ src/components/ProtectedRoute/index.js
@@ -0,0 +1,16 @@
+import {Redirect,Route} from 'react-router-dom'
+import Cookies from 'js-cookie'
+import Home from '../Home'
+
+const ProtectedRoute = () => {
+    const token = Cookies.get('jwt_token')
+    if(token === undefined){
+        return <Redirect to="/login" />
+    }
+    return <Route exact path="/" component={Home} />
+
+}
+
+export default ProtectedRoute
+
+
